d5838e50544d8bd157c246cc2b9928833d0800be
chayanikabhandary <chayanika.bhandary@gmail.com>
2018-01-24 18:31:33 +0530

    Initial commit

--- app/models.py
+++ app/models.py
@@ -7,8 +7,10 @@ class Task(db.Model):
     created_by = db.Column(db.String(128), nullable= False)
     status = db.Column(db.Boolean)
     deleted_by = db.Column(db.String(128))
-    created_at = db.Column(db.Date, nullable= False)
-    deleted_at = db.Column(db.Date, nullable= True)
+    created_at = db.Column(db.Text, nullable= False)
+    deleted_at = db.Column(db.Text, nullable= True)
 
     def __repr__(self):
         return '<Task {}>'.format(self.task_description)
+
+     t= Task(task_description= 'Todo list', due_date= datetime.date(2018,01,30), created_by= 'Chayanika', created_at= datetime.date(2018,01,24))   
